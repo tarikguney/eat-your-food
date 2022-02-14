513176d7eb837a34804285bb729de83fb736fa8e
Tarik Guney <atarikguney@gmail.com>
2022-02-13 21:51:33 -0700

    Changing the text of enable button to disable based on the current status

--- popup.js
+++ popup.js
@@ -1,10 +1,27 @@
 let enableInterruptionButton = document.getElementById("enableInterruption");
 
+chrome.storage.local.get("interruptionEnabled").then(async (result) => {
+    let final = result || {interruptionEnabled: false};
+    console.log(final)
+
+    if (final.interruptionEnabled) {
+        enableInterruptionButton.innerText = "Disable";
+    } else {
+        enableInterruptionButton.innerText = "Enable";
+    }
+});
+
 enableInterruptionButton.addEventListener("click", async () => {
 
-    let result = await chrome.storage.local.get("interruptionEnabled") || false;
+    let result = await chrome.storage.local.get("interruptionEnabled") || {interruptionEnabled: false};
 
     await chrome.storage.local.set({"interruptionEnabled": !result.interruptionEnabled})
+
+    if (!result.interruptionEnabled) {
+        enableInterruptionButton.innerText = "Disable";
+    } else {
+        enableInterruptionButton.innerText = "Enable";
+    }
 
     let [activeTab] = await chrome.tabs.query({currentWindow: true, active: true});
     let message = {enabled: !result.interruptionEnabled};
