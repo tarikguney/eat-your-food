addd04fbdee4d73a948949d089a9fc7a5ef0bcb2
Tarik Guney <atarikguney@gmail.com>
2022-02-20 20:31:09 -0700

    Added multitab support

--- background.js
+++ background.js
@@ -12,28 +12,22 @@ chrome.runtime.onInstalled.addListener(async details => {
     if (!pauseConfig.interruptedTabs) {
         await chrome.storage.sync.set({interruptedTabs: []});
     }
-
-    chrome.storage.sync.get(function(result){console.log(result)})
 });
 
 chrome.tabs.onRemoved.addListener(async function (tabId, removed) {
 
-    let interruptionConfig = await chrome.storage.local.get("interruptedTabs");
-    let interruptedTabIndex = interruptionConfig.interruptedTabs
+    let interruptedTabs = (await chrome.storage.sync.get("interruptedTabs")).interruptedTabs;
+    let interruptedTabIndex = interruptedTabs
         .findIndex(a => a.tabId === tabId && a.windowId === removed.windowId);
 
-    let currentState = interruptedTabIndex > -1;
-
-    if (currentState) {
-        interruptionConfig.interruptedTabs.splice(interruptedTabIndex, 1);
-        await chrome.storage.local.set({"interruptedTabs": interruptionConfig.interruptedTabs});
+    if (interruptedTabIndex > -1) {
+        interruptedTabs.splice(interruptedTabIndex, 1);
+        await chrome.storage.sync.set({"interruptedTabs": interruptedTabs});
     }
 });
 
 chrome.windows.onRemoved.addListener(async function (windowId) {
-
-    // todo: Check if any element exists before removing and re-saving.
-    let interruptionConfig = await chrome.storage.local.get("interruptedTabs");
-    _.remove(interruptionConfig.interruptedTabs, n => n.windowId === windowId);
-    await chrome.storage.local.set({"interruptedTabs": interruptionConfig.interruptedTabs});
+    let interruptedTabs = (await chrome.storage.sync.get("interruptedTabs")).interruptedTabs;
+    let remaining = interruptedTabs.find(a => a.windowId !== windowId);
+    await chrome.storage.sync.set({"interruptedTabs": remaining});
 });
